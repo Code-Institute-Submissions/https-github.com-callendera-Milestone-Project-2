9a2a1ef0401ac462759f69e4b186a8de939d0e0f
EC2 Default User <ec2-user@ip-172-31-43-242.us-east-2.compute.internal>
2019-08-20 18:25:21 +0000

    bug fix in application js

--- assets/js/sendApplication.js
+++ assets/js/sendApplication.js
@@ -1,17 +1,12 @@
-function sendMail(volunteerForm) {
-    emailjs.send("outlook", "subscribe", {
-        "from_name": volunteerForm.fullname.value,
-        "from_email": volunteerForm.emailaddress.value,
-        "subscriber_message": volunteerForm.subscribermessage.value,
-        
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}
+emailjs.sendForm('outlook', 'subscribe', '#volunteerForm')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+
+
+
+
+
