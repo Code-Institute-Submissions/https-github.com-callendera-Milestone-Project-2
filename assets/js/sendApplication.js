9f7304d7745e585250abb7f1411258aacd2b8c36
EC2 Default User <ec2-user@ip-172-31-43-242.us-east-2.compute.internal>
2019-08-21 20:17:27 +0000

    quick update to check over code

--- assets/js/sendApplication.js
+++ assets/js/sendApplication.js
@@ -1,10 +1,10 @@
+
 emailjs.sendForm('outlook', 'subscribe', '#volunteerForm')
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
        console.log('FAILED...', error);
     });
-
 
 
 
