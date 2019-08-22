4f560398e37a2931d19ffc26292255dfe053f137
EC2 Default User <ec2-user@ip-172-31-43-242.us-east-2.compute.internal>
2019-08-22 16:36:45 +0000

    quick save

--- assets/js/sendApplication.js
+++ assets/js/sendApplication.js
@@ -1,11 +1,28 @@
+var myform = $("form#volunteerForm");
+volunteerForm.submit(function(event){
+	event.preventDefault();
 
-emailjs.sendForm('outlook', 'subscribe', '#volunteerForm')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
+	var params = volunteerForm.serializeArray().reduce(function(obj, item) {
+     obj[item.name] = item.value;
+     return obj;
+  }, {});
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+
+  var template_id = "subscribe";
+  volunteerForm.find("button").text("Sending...");
+  emailjs.send(service_id, template_id, params)
+  	.then(function(){ 
+       alert("Sent!");
+       volunteerForm.find("button").text("Send");
+     }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
     });
 
+  return false;
+});
 
 
 
