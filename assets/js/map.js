9967dcc1866bda58c008fba37161cc41c849749d
EC2 Default User <ec2-user@ip-172-31-43-242.us-east-2.compute.internal>
2019-08-26 15:23:04 +0000

    added comments for js files

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,14 +1,16 @@
+// function that loads the map for the events
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 14,
         center: {
+            // gives the area for which the map should be focussed 
             lat: 39.536560,
             lng: -83.438750
         }
     });
-
+    //labels for markers
     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-
+    //latitude and longitude of the each event location
     var locations = [{
         lat: 39.537121,
         lng: -83.438751
@@ -19,13 +21,14 @@ function initMap() {
         lat: 39.551048,
         lng: -83.422234
     }];
-
+        // produces markers
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
+    //allows for a marker cluster when map is zoomed out enough
     var markerCluster = new MarkerClusterer(map, markers, {
         imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
     });
--- assets/js/sendApplication.js
+++ assets/js/sendApplication.js
@@ -1,3 +1,4 @@
+// targets volunteer application
 var volunteerForm = $("form#volunteerForm");
 volunteerForm.submit(function(event){
 	event.preventDefault();
@@ -7,16 +8,18 @@ volunteerForm.submit(function(event){
      return obj;
   }, {});
 
-
+    //service where form will be sent
   var service_id = "outlook";
-
+//helps set parameters for the form in template id
   var template_id = "subscribe";
+  // good ux for user to know when the form is sending and has been sent. a message pops up to confirmt the form was sent
   volunteerForm.find("button").text("Sending...");
   emailjs.send(service_id, template_id, params)
   	.then(function(){ 
        alert("Sent!");
        volunteerForm.find("button").text("Send");
      }, function(err) {
+         //error message that will appear with an error
       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
       volunteerForm.find("button").text("Send");
     });
